402c14d8a35af6a6fea7e710685d7a888c006d53
haja <87018429+haritsoa@users.noreply.github.com>
2021-09-15 12:02:22 +0300

    main

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from 'react'
+import './chat.css'
+import   'react-bootstrap/Button';
+import Img from './images/profiles/ben.png'
+import Img1 from './images/profiles/daryl.png'
+import './bootstrap/css/bootstrap.css'
+import {Row} from "react-bootstrap";
+import {InputGroup} from "react-bootstrap";
+import FormControl from 'react-bootstrap/FormControl';
+import {Button} from 'react-bootstrap'
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <head>
+
+           <title>Chat App</title>
+           <link rel="stylesheet" type="text/css" media="screen" href="css/chat.css" />
+           
+      </head>
+      
+       <div className="App-Title">
+
+            <h1 className="title">Message</h1>
+        </div>
+      <body>   
+            <div id="chat-window">
+                <h2>Chat Messages</h2>
+                <div id="container">
+                <img src={Img} alt="ben"/>
+                  <p>Bonjour. Est ce que ca va??</p>
+                  <span id="time-right">11:00</span>
+                </div>
+
+                 <div id="containery">
+                  <img src={Img1} alt="daryl" />
+                    <p>Salut! oui je vais bien. 
+                      Peut tu sortir Aujourd'hui?
+                    </p>
+                    <span id="time-right">11:01</span>
+                  </div>
+            
+                  <div id="container">
+                <img src={Img} alt="ben"/>
+                  <p>Oui je peut, mais ou allons nous?</p>
+                  <span id="time-right">11:02</span>
+                </div>
+
+                <div id="containery">
+                  <img src={Img1} alt="daryl" />
+                  <p>Chez le restaurant, parceque j'ai un ticket de 
+                    5000f
+                  </p>
+                  <span id="time-left">11:05</span>
+                </div>
+                <Row style={{marginLeft:'-22em'}}>
+                    <InputGroup>
+                      <InputGroup.Text>Ecrire ici Votre Message</InputGroup.Text>
+                            <FormControl as="textarea" aria-label="With textarea" />
+                            <Button variant="primary">Envoyer</Button>
+                      </InputGroup>
+              </Row>  
+
+            </div>
+           
+      </body>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,134 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './chat.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Card from 'react-bootstrap/Card'
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import {Container} from "react-bootstrap";
+import './App.css';
+import { Button } from 'react-bootstrap';
+import Img from './images/profiles/ben.png'
+import Img3 from './images/profiles/jacob.png'
+import Img4 from './images/profiles/john.jpeg'
+import Img1 from './images/profiles/daryl.png'
+import Img2 from './images/profiles/douglas.png'
+
+import {Row} from 'react-bootstrap';
+import {Form} from 'react-bootstrap'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+
+      <Container>
+        <Navbar fixed="top" className="navbar navbar-expand-lg navbar-light  bg-primary">
+          <Navbar.Brand href="#">ANDRIATSILANIARIVO Hajaharitsoa IMTIC3 N: 24</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="#">Acceuill</Nav.Link>
+            <Nav.Link href="#">Utilisateurs</Nav.Link>
+            <Nav.Link href="#" ><Button>Deconexion</Button></Nav.Link>
+            <Nav.Link href="#"><Button>Ajout contact</Button></Nav.Link>
+          </Nav>
+        </Navbar>
+      </Container>
+
+    <Container fluid style={{display:'flex', flexDirection:'column'}}>
+      <Row>
+      <div id="side-bar1" style={{textAlign:'center',display:'inline-block',height:'400px'}}>
+                 
+                 <div className="headerOfSide">
+                      <h1 className="userlist">
+                             Inscrire
+                      </h1>                 
+                  </div>   
+                  <Card className ="" style={{ backgroundColor:' rgba(33, 126, 95, 0.192);',width: '18rem',marginLeft:'15px'}}>
+                        <Form style= {{alignContent:'center',borderRadius:'30px'}} >
+                          <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Text className="text-muted">
+                              We'll never share your email with anyone else.
+                            </Form.Text>
+                          </Form.Group>
+
+                          <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                          </Form.Group>
+                          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="Check me out" />
+                          </Form.Group>
+                            <div className="buton-spacin">
+                              <Button className="button" variant="primary" type="submit">
+                                Inscrire
+                              </Button>
+                              <Button className="button" variant="primary" type="submit">
+                                Connecter
+                              </Button>
+                            </div>
+                    </Form>
+
+                </Card>
+      </div>
+
+    
+      ,
+      <div id="chat-container" style = {{marginTop:'5em'}} >
+          <div id="side-bar">
+                 
+                 <div className="headerOfSide">
+                      <p>
+                          <form>
+                              <input type="text" name="search" placeholder="Search.."/>
+                              <h4 className="userlist">
+                            Listes des utilisateurs: 
+                              </h4> 
+                          </form>
+                      </p>
+                                      
+                  </div>   
+                  <Card  className="listcard" style={{ width: '24rem',marginLeft:'15px',marginBlock:'20px',marginRight:'13px' }}>
+                     <div  className="div">
+                            <ul className="ul">
+                              <li className="li">
+                                <img src={Img1} alt="" />
+                                <h3 className="h3">AVDAR</h3>
+                                <p className="p"> Avdar@gmail.com</p>
+                              </li>
+                              <li className="li">
+                                <img src={Img} alt="" />
+                                <h3 className="h3">RAMAL</h3>
+                                <p className="p">Ramal@gmail.com</p>
+                              </li>
+                              <li className="li">
+                                <img src={Img3} alt="" />
+                                <h3 className="h3">MENK</h3>
+                                <p className="p"> Menk@gmail.com</p>
+                              </li>
+                              <li className="li">
+                                <img src={Img2} alt="" />
+                                <h3 className="h3">ADALAH</h3>
+                                <p className="p"> Abdala@gmail.com</p>
+                              </li>
+                              <li className="li">
+                                <img src={Img4} alt="" />
+                                <h3 className="h3">Rodrigo</h3>
+                                <p className="p"> rodrigo@gmail.com</p>
+                              </li>
+                            </ul>
+                          </div>
+                </Card>
+         </div>
+      
+       <App />
+       </div>
+       </Row>
+       
+      </Container>
+     
+
   </React.StrictMode>,
   document.getElementById('root')
 );
